5bb8ff8c9d1fc477a90a14cffd354801df7e7798
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-20 16:40:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,17 @@
-class Transfer
-  # your code here
+class Project
+
+attr_accessor :title, :backers
+attr_reader
+
+
+  def initialize(title="")
+    @title=title
+    @backers = []
+  end
+
+  def add_backer(name)
+      @backers << name
+      name.backed_projects << self
+      # name.b_project(self)
+  end
 end
