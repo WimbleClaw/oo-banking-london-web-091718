b868959e14958b155745395deec5e41079a3a62c
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-20 16:41:21 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,17 +1,35 @@
-class Project
+class Transfer
+  attr_accessor :sender, :receiver, :amount, :status, :sent_amount
 
-attr_accessor :title, :backers
-attr_reader
-
-
-  def initialize(title="")
-    @title=title
-    @backers = []
+  def initialize(from, to, amount)
+    @sender = from
+    @receiver = to
+    @amount = amount
+    @status = "pending"
+    @sent_amount = 0
   end
 
-  def add_backer(name)
-      @backers << name
-      name.backed_projects << self
-      # name.b_project(self)
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if valid?
+      @sent_amount = @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+      @amount = 0
+    end
+    if @sender.valid? == false
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    @sender.balance += @sent_amount
+    @receiver.balance -= @sent_amount
+    @status = "reversed"
   end
 end
